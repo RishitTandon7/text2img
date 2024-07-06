f91e764cb7e97e40e34cc4e6eaa5b9252d340a19
Rishit Tandon <142263549+RishitTandon7@users.noreply.github.com>
2024-07-06 21:38:54 +0530

    Server request

--- Text_to_img/ImgGen.js
+++ Text_to_img/ImgGen.js
@@ -13,12 +13,22 @@ async function query() {
             body: JSON.stringify({ "inputs": inputTxt.value }),
         }
     );
-    const result = await response.blob();
-    return result;
+    if (response.ok) {
+        const result = await response.blob();
+        return result;
+    } else {
+        throw new Error('Limit of the users using the website is reached. Please try after some time sorry for the inconvenience caused.');
+    }
 }
 
 button.addEventListener('click', async function () {
-    const response = await query();
-    const objectURL = URL.createObjectURL(response);
-    image.src = objectURL;
+    try {
+        const response = await query();
+        const objectURL = URL.createObjectURL(response);
+        image.src = objectURL;
+    } catch (error) {
+        image.src = "";
+        image.alt = error.message;
+        image.classList.add("error-message");
+    }
 });
